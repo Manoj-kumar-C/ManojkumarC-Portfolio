97a4f173ddded77c63c211af4197e17ad6ce4333
manoj-chandran <developermanoj2003@gmail.com>
2023-12-18 20:38:19 +0530

    some changes

--- src/containers/skills/CloudInfraImg.js
+++ src/containers/skills/CloudInfraImg.js
@@ -3,6 +3,6 @@ import React, { Component } from "react";
 export default class CloudInfraImg extends Component {
   render() {
     const theme = this.props.theme;
-    return <img src={require("../../assests/home/FullStackDev.png")} />;
+    return <img src={require("../../assests/home/cloud-computing.png")} />;
   }
 }
--- src/containers/skills/DataScienceImg.js
+++ src/containers/skills/DataScienceImg.js
@@ -3,6 +3,6 @@ import React, { Component } from "react";
 export default class DataScienceImg extends Component {
   render() {
     const theme = this.props.theme;
-    return <img src={require("../../assests/home/FullStackDev.png")} />;
+    return <img src={require("../../assests/home/digital_marketing.png")} />;
   }
 }
