af889c315b7b9372fc601eb631a3e1bceae17ef9
manoj-chandran <developermanoj2003@gmail.com>
2023-12-18 20:27:46 +0530

    image

--- src/containers/greeting/FeelingProud.js
+++ src/containers/greeting/FeelingProud.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class FeelingProud extends Component {
   render() {
-    return <img src={require("../../assests/projects.jpg")} />;
+    return <img src={require("../../assests/home/FullStackDev.png")} />;
   }
 }
 
