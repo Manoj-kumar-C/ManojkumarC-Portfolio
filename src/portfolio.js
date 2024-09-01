cd189463690be7a1001392a8d8b503e1a89dd2e3
Manojkumar C (Fresh Spar Technologies) <developermanoj2003@gmail.com>
2024-09-01 13:51:26 +0530

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -24,7 +24,7 @@ const greeting = {
   title: "Manojkumar C",
   logo_name: "ManojKumar",
   nickname: "manojkumar_c",
-  subTitle: "Full Stack Developer , Digital Marketer, Cloud & DevOps Engineer",
+  subTitle: "Full Stack Developer , Digital Marketer, Cloud & DevOps Engr",
   resumeLink:
     "https://drive.google.com/file/d/1Alld2VUb_ViUNBU9FewCEVSQ0nau91Ij/view?usp=sharing",
   portfolio_repository: "https://github.com/Manoj-kumar-C",
